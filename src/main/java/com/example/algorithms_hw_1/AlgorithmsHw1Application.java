90e7d20ba7d1c306a763a57090a602645821af80
Dima <akmeev2011@yandex.ru>
2022-12-02 19:32:00 +0300

    Initial commit

---
+++ src/main/java/com/example/algorithms_hw_1/AlgorithmsHw1Application.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_hw_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsHw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsHw1Application.class, args);
+    }
+
+}
